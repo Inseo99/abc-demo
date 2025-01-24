df6e39061bc7dc114e6c74791982ea8e33ccb183
Inseo99 <jjj990626@naver.com>
2025-01-24 14:36:02 +0900

    jstl까지 적용

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.ArticleService;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Article;
+import com.example.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -16,74 +19,78 @@ public class UsrArticleController {
 	@Autowired
 	ArticleService articleService;
 
-//	생성자
-	public UsrArticleController() {
-
-	}
-
-	// 서비스 메서드
-
-	// 액션 메서드
-
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 
 		Article article = articleService.getArticleById(id);
 
 		if (article == null) {
-			return id + "번 그런거없어";
-
+			return ResultData.from("F-1", Ut.f("%d번 글은 없어", id));
 		}
 
-		return article;
+		article = articleService.getArticleById(id);
+
+		return ResultData.from("S-1", Ut.f("%d번 게시글 작성", id), article);
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Object doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 
 		Article article = articleService.getArticleById(id);
 
 		if (article == null) {
-			return id + "번 그런거없어";
+			return ResultData.from("F-1", Ut.f("%d번 글은 없어", id));
 
 		}
 
-		article.setTitle(title);
-		article.setBody(body);
-
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시글 수정", id), article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData doDelete(int id) {
 
 		Article article = articleService.getArticleById(id);
 
 		if (article == null) {
-			return id + "번 그런거없어";
+			return ResultData.from("F-1", Ut.f("%d번 글은 없어", id));
 		}
 
-//		articleService.deleteArticle(article);
 		articleService.deleteArticle(id);
 
-		return id + "번 글 삭제";
+		return ResultData.from("S-1", Ut.f("%d번 게시글 삭제", id));
 	}
 
-	@RequestMapping("/usr/article/doAdd")
+	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
-		Article article = articleService.writeArticle(title, body);
-		return article;
+	public ResultData doWrite(String title, String body) {
+
+		if (Ut.isEmptyOrNull(title)) {
+			return ResultData.from("F-1", "제목 입력 안함");
+		}
+
+		if (Ut.isEmptyOrNull(body)) {
+			return ResultData.from("F-2", "내용 입력 안함");
+		}
+
+		ResultData writeArticleRd = articleService.writeArticle(title, body);
+
+		int id = (int) writeArticleRd.getData1();
+
+		Article article = articleService.getArticleById(id);
+
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
 	}
 
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
+	@RequestMapping("/usr/article/list")
+	public String showList(Model model) {
 
-		return articleService.getArticles();
+		List<Article> articles = articleService.getArticles();
+		model.addAttribute("articles", articles);
+
+		return "usr/article/list";
 	}
 
 }
