749f56b5e46b8a92c10b58077d8c114103043afb
Inseo99 <jjj990626@naver.com>
2025-01-24 11:06:58 +0900

    test

---
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -0,0 +1,89 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.service.ArticleService;
+import com.example.demo.vo.Article;
+
+@Controller
+public class UsrArticleController {
+
+	@Autowired
+	ArticleService articleService;
+
+//	생성자
+	public UsrArticleController() {
+
+	}
+
+	// 서비스 메서드
+
+	// 액션 메서드
+
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 그런거없어";
+
+		}
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 그런거없어";
+
+		}
+
+		article.setTitle(title);
+		article.setBody(body);
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 그런거없어";
+		}
+
+//		articleService.deleteArticle(article);
+		articleService.deleteArticle(id);
+
+		return id + "번 글 삭제";
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = articleService.writeArticle(title, body);
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articleService.getArticles();
+	}
+
+}
